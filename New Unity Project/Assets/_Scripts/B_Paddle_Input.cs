27a9a80cecb7a1b2ee846265cae9086112afc88a
Aidan Robinson <aidanrobinson6@gmail.com>
2019-08-20 11:48:28 +1200

    First_Update

---
+++ New Unity Project/Assets/_Scripts/B_Paddle_Input.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class B_Paddle_Input : MonoBehaviour
+{
+    public float speed;
+    public float clampValue;
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovePaddle(Input.GetAxis("Horizontal"));
+    }
+
+
+    public void MovePaddle(float amountToMove)
+    {
+        Vector3 clampedGameObjectPosition = gameObject.transform.position + new Vector3(-amountToMove * speed, 0, 0);
+
+        clampedGameObjectPosition.x = Mathf.Clamp(clampedGameObjectPosition.x, -clampValue, clampValue);
+
+        gameObject.transform.position = clampedGameObjectPosition;
+    }
+}
---
+++ New Unity Project/Assets/_Scripts/Ball_Behaviour.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball_Behaviour : MonoBehaviour
+{
+    private Rigidbody rb;
+    public float thrust;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+
+        rb.AddForce(transform.forward * thrust, ForceMode.Acceleration);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
